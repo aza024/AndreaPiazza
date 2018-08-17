4c24e2d68ff0da8110c2f51c169b8850a0262493
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-08-17 15:29:39 -0700

    added JS and GoogleMaps

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"
-src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+
+
+function initialize(){
+    var mapProperties = {
+        center:new google.maps.LatLng(37.790840,-122.401160),
+        zoom: 17,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map=new google.maps.Map(document.getElementById("googleMap"), mapProperties);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+// var myCenter = google.maps.LatLng(37.790840,-122.401160);
+var marker=new google.maps.Marker({
+    position:LatLng(37.790840,-122.401160), 
+    animation:google.maps.Animation.BOUNCE, //animates the marker
+    // icon:'pinkball.png' // replace default marker with custom icon
+    });
+  marker.setMap(map);
